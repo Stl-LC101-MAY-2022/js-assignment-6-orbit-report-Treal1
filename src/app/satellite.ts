92005a33eba3ebf0f9e0763ecd2b12033b4abbd4
Torrie Real <torijames1997@gmail.com>
2022-08-07 11:05:14 -0500

    -m "added orbit list tag an made sure that list was visible on the server"

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -14,9 +14,10 @@ export class Satellite {
 		this.operational = operational;
    }
 	
-	isSpaceDebris(): boolean {
-		return true;
-   }
+   isSpaceDebris(): boolean {
+	if (this.type == "Space Debris") { return true; }
+	else { return false; }
+}
 
 }
 
